9ad0e0453182dfb6454c3bbe7848e18bab048d9f
Duncan DeNiro <duncandeniro@gmail.com>
2021-07-24 22:40:50 -0600

    refactored main loop and removed unecessary code. and added comments throughout code.

--- notify.py
+++ notify.py
@@ -6,6 +6,7 @@ from infi.systray import SysTrayIcon
 # def say_hello(systray):
 #     print("Hello, World!")
 
+# Function which exits program, accessible by right clicking the systray
 def onQuit(systray):
     os._exit(1)
 
@@ -17,8 +18,6 @@ session_time = 5 * 60 # Number of minutes to work.
 break_time = 1 * 60 # Number of minutes break.
 
 #Create each of our notifcation objects to send to Desktop using winotify library.
-notif_endBreak = Notification(app_id="Pomodoro App", title="Break ended!", msg="Time to get back to work!", icon=r"a:/PomoTimer/terminal.png")
-notif_endBreak.set_audio(audio.Mail, loop=False)
 
 notif_startBreak = Notification(app_id="Pomodoro App", title=f"Work session {session_count} ended!", msg="Time to take a break.", icon=r"a:/PomoTimer/terminal.png")
 notif_startBreak.set_audio(audio.Mail, loop=False)
@@ -27,22 +26,23 @@ notif_startSession = Notification(app_id="Pomodoro App", title=f"Work session {s
 notif_startSession.set_audio(audio.Mail, loop=False)
 
 try:
+    #Create the system tray icon
     systray = SysTrayIcon("icon.ico", "Pomodoro Timer", on_quit=onQuit)
     systray.start()
-    
-    choice = 1
 
-    while int(choice) != 0:
+    #Main Pomodoro loop.
+    while True:
 
+        #Start the Pomodoro work session
         notif_startSession.build().show()
         time.sleep(session_time)
         
         session_count += 1
 
+        #Start break session
         notif_startBreak.build().show()
         time.sleep(break_time)
 
-    systray.shutdown()
 except (KeyboardInterrupt, SystemExit):
     systray.shutdown()
     os._exit(1)
