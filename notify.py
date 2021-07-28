d4af3e532a7de7e12841a819a3ebb8067eb0ecbd
ddeniro <duncandeniro@gmail.com>
2021-07-27 18:54:57 -0600

    update systray hover text and upd. session # disp.

--- notify.py
+++ notify.py
@@ -3,15 +3,22 @@ import time, sys, os
 import pkg_resources
 from infi.systray import SysTrayIcon
 
-# def say_hello(systray):
-#     print("Hello, World!")
-
-# Function which exits program, accessible by right clicking the systray
+def say_hello(systray):
+    print("Hello, World!")
+#Function which exits program, accessible by right clicking the systray
 def onQuit(systray):
     os._exit(1)
 
-#menu_options = (("Say Hello", "test", say_hello), )
+# incrementTime(): increments the number of minutes 
 
+def testing():
+    pass
+
+menu_options = (("Say Hello", None, say_hello),
+                ("testing", None, testing))
+
+session_times = [5, 15, 30, 45, 60]
+break_times = [1, 2.5, 5, 10]
 
 session_count = 1 # number of sessions continuously tracked.
 session_time = 5 * 60 # Number of minutes to work.
@@ -27,20 +34,24 @@ notif_startSession.set_audio(audio.Mail, loop=False)
 
 try:
     #Create the system tray icon
-    systray = SysTrayIcon("icon.ico", "Pomodoro Timer", on_quit=onQuit)
+    systray = SysTrayIcon("icon.ico", "Pomodoro Timer", on_quit=onQuit, menu_options=menu_options)
     systray.start()
-
     #Main Pomodoro loop.
     while True:
 
         #Start the Pomodoro work session
         notif_startSession.build().show()
+        systray.update(hover_text='Work Session')
         time.sleep(session_time)
         
         session_count += 1
 
         #Start break session
         notif_startBreak.build().show()
+        systray.update(hover_text='Break Session')
+
+        notif_startBreak.title = f"Work session {session_count} ended!"
+        notif_startSession.title=f"Work session {session_count} started."
         time.sleep(break_time)
 
 except (KeyboardInterrupt, SystemExit):
